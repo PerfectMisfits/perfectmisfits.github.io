e3149069a6cc54ec29489b40bd4588fb49c5d7c1
Michael Osajeh <osajehmichael@gmail.com>
2022-02-06 01:08:25 +0100

    Added sidebar navigation for smaller screens

---
+++ index.js
@@ -0,0 +1,9 @@
+function openSideBar() {
+  document.getElementById("main-menu").style.width = "40%";
+  document.getElementById("menu-list").style.display = "block";
+}
+
+function closeSideBar() {
+  document.getElementById("main-menu").style.width = "0%";
+  document.getElementById("menu-list").style.display = "none";
+}
